4584714a009f5a36cddac3ab34cd3b7c7990ea62
Stefan Meeuwessen <StefanMeeuwessen@outlook.com>
2024-04-18 22:51:25 +0200

    First draft

---
+++ Bowser.py
@@ -0,0 +1,60 @@
+import os
+import datetime
+import logging
+from flask import Flask, render_template, request
+
+# Set the root path to the directory containing the Python script
+root_path = os.path.dirname(os.path.abspath(__file__))
+app = Flask(__name__, root_path=root_path)
+
+# Configure logging
+log_folder = os.path.join(root_path, 'Logs/Bowser_Logs')
+os.makedirs(log_folder, exist_ok=True)
+log_file_name = os.path.join(log_folder, f"{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{app.name}.log")
+logging.basicConfig(filename=log_file_name, level=logging.INFO, format='%(asctime)s %(levelname)s in %(name)s: %(message)s')
+
+# Function to get the list of directories under a given path with their statuses
+def get_directories_with_status(path):
+    path = os.path.join(root_path, path)  # Construct absolute path relative to root_path
+    directories = {}
+    for dir_name in os.listdir(path):
+        abs_path = os.path.join(path, dir_name)
+        status = "enabled" if "Enabled" in path else "disabled"
+        directories[dir_name] = status
+    return directories
+
+# Function to get the list of directories under Enabled and Disabled folders
+def get_script_directories():
+    enabled_path = 'PyRocesses/Enabled'
+    disabled_path = 'PyRocesses/Disabled'
+    
+    enabled_dirs = get_directories_with_status(enabled_path)
+    disabled_dirs = get_directories_with_status(disabled_path)
+    return enabled_dirs, disabled_dirs
+
+# Route to display the list of scripts
+@app.route('/')
+def index():
+    # Log the request
+    log_request(request)
+    
+    directories = {}
+    enabled_dirs, disabled_dirs = get_script_directories()
+    directories.update(enabled_dirs)
+    directories.update(disabled_dirs)
+    logging.info('Displayed directory list')
+    return render_template('index.html', directories=directories)
+
+# Function to log the request
+def log_request(req):
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    ip_address = req.remote_addr
+    http_method = req.method
+    url = req.url
+    status_code = 200  # Assuming success by default
+    user_agent = req.user_agent.string
+    log_entry = f"[{timestamp}] {ip_address} - {http_method} {url} - {status_code} - {user_agent}\n"
+    logging.info(log_entry)
+
+if __name__ == '__main__':
+    app.run(debug=True)
